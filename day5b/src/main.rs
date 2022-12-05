15820705a898a9752fa15ffb0cfbe0a6a233bfce
Linus Kardell <linus.kardell@gmail.com>
2022-12-05 19:55:02 +0100

    Add day 5

---
+++ day5b/src/main.rs
@@ -0,0 +1,37 @@
+use std::io::Read;
+
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+
+    let mut lines = input.lines();
+
+    let stack_lines: Vec<_> = (&mut lines).take_while(|line| !line.chars().nth(1).unwrap().is_ascii_digit()).collect();
+    let mut stacks = Vec::with_capacity(9);
+    for line in stack_lines.into_iter().rev() {
+        for (n, c) in line.chars().skip(1).step_by(4).enumerate() {
+            if c.is_ascii_alphabetic() {
+                while stacks.len() <= n {
+                    stacks.push(Vec::with_capacity(20));
+                }
+                stacks[n].push(c);
+            }
+        }
+    }
+
+    for line in lines.skip(1) {
+        let mut nums = line.split_whitespace().skip(1).step_by(2).map(|x| x.parse::<usize>().unwrap());
+        let count = nums.next().unwrap();
+        let src = nums.next().unwrap() - 1;
+        let dst = nums.next().unwrap() - 1;
+        let src_new_len = stacks[src].len() - count;
+
+        for i in 0..count {
+            let val = stacks[src][src_new_len + i];
+            stacks[dst].push(val);
+        }
+        stacks[src].truncate(src_new_len);
+    }
+
+    println!("{}", stacks.into_iter().map(|x| *x.last().unwrap()).collect::<String>());
+}
